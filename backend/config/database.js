725b81a09a2356b0e9307dc90168514648357bfb
exuviadot <rabbanidwiki@gmail.com>
2026-03-17 12:04:56 +0700

    Revert "update: biar di vm ga error"
    This reverts commit cc8800f50d401d06d908eecb5de0cd64ec2bb48d.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,7 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
-    {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize('notes_db', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql',
     logging: false
 });
