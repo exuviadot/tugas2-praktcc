4a71cfe31077c23487be51f9c76c3c79b7c0e249
exuviadot <rabbanidwiki@gmail.com>
2026-03-17 10:38:49 +0700

    update: biar sekali run, frontend jg ikut ke-run

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
+const path = require('path');
 const sequelize = require('./config/database');
 const noteRoutes = require('./routes/noteRoutes');
 
 const app = express();
 const cors = require('cors');
 
-app.use(cors({
-  origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Jika butuh kirim cookie/session
-}));
-
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '..', 'frontend'), {
+    index: 'index.html'
+}));
+
+// app.get('/api', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 require('./schema/Notes');
 app.use('/api/v1/notes', noteRoutes);
@@ -23,5 +23,7 @@ app.use('/api/v1/notes', noteRoutes);
 const port = process.env.PORT || 3000;
 sequelize.sync().then(() => {
     console.log('Database synced');
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
 });
