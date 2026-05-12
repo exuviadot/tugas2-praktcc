00a56e1a87643c099e613851523694097fd37f42
exuviadot <rabbanidwiki@gmail.com>
2026-05-12 12:38:43 +0700

    fix: ubah struktur server dan konfigurasi express untuk deployment

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,35 @@
+require('dotenv').config();
+
 const express = require('express');
-const path = require('path');
 const sequelize = require('./config/database');
 const noteRoutes = require('./routes/noteRoutes');
 
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '..', 'frontend'), {
-    index: 'index.html'
-}));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
-// app.get('/api', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.redirect('/index.html');
+});
 
-require('./schema/Notes');
 app.use('/api/v1/notes', noteRoutes);
 
 const port = process.env.PORT || 3000;
-sequelize.sync().then(() => {
-    console.log('Database synced');
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+sequelize
+    .sync()
+    .then(() => {
+        console.log('Database synced');
+    })
+    .catch((error) => {
+        console.error('Error syncing database:', error);
     });
+
+app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
 });
