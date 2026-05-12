df16d272a65b2a814d68734e9c18a8edbdd5e683
exuviadot <rabbanidwiki@gmail.com>
2026-05-12 12:30:55 +0700

    chore: ubah dari localhost menjadi env variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('notes_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        timezone: '+07:00',
+        logging: false
+    }
+);
 
 module.exports = sequelize;
